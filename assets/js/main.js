5c8836ff8d1b4933cecf87083ece6d50704275ec
marcecabelloherrera <marcecabello.h@gmail.com>
2017-08-05 10:33:48 -0400

    slider mobile

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+//activar boton flotante
+$(document).ready(function(){
+
+  $('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
+
+});
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
+       
