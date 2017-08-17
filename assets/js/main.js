0c7f5090844a7021557aa8697609143123663364
marcecabelloherrera <marcecabello.h@gmail.com>
2017-08-17 15:17:14 -0300

    arreglo nav mobile, arreglo parallax mobile, arreglo efecto logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,10 @@ $(document).ready(function(){
 
   $('.fixed-action-btn').openFAB();
   $('.fixed-action-btn').closeFAB();
-
+  $('.slider').slider();
+ 
 });
 
-
-  $(document).ready(function(){
-      $('.button-collapse').sideNav();
-      $('.slider').slider();
- 
-    });
 
    $(document).ready(function(){
       $('.parallax').parallax();
